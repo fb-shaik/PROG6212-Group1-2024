8aae4234326f34f87635265babd122dc21c6d682
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-09-02 21:44:56 +0200

    Add files via upload

---
+++ Employee_Attributes_demo/Employee_Attributes_demo/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Attributes_demo
+{
+     class Employee
+    {
+        private string id;
+        private string fName;
+        private string sName;
+        private string email;
+        private string phone;
+        private double salary;
+
+        [Required(ErrorMessage = "ID is required")]
+        [StringLength(13, MinimumLength = 13, ErrorMessage = "ID must be 13 characters")]
+        public string Id { get => id; set => id = value; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FName { get => fName; set => fName = value; }
+
+        [Required(ErrorMessage = "Surname Name is required")]
+        public string SName { get => sName; set => sName = value; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get => email; set => email = value; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number must be 10 characters")]
+        public string Phone { get => phone; set => phone = value; }
+
+        [Range(1, 1000000, ErrorMessage = "Salary must be between R1 & R1 mil")]
+        public double Salary { get => salary; set => salary = value; }
+
+        [Obsolete("This method is obsolete. Consider UpdatedGetValues")]
+        public string getValues() 
+        {
+            return this.fName;
+        }
+
+        public string updatedGetValues()
+        {
+            return (this.fName + " " + sName);
+        }
+
+    }
+}
---
+++ Employee_Attributes_demo/Employee_Attributes_demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Attributes_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Id = "23345";
+            emp.FName = "Joe";
+            emp.SName = "Blog";
+            emp.Email = "joe.blog@gmail.com";
+            emp.Phone = "123456";
+            emp.Salary = 10000;
+
+            Console.WriteLine(emp.getValues());
+
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            ValidationContext context = new ValidationContext(emp);
+
+            bool validCheck = Validator.TryValidateObject(emp, context, validationResults, true);
+
+            if (!validCheck)
+            {
+                foreach (ValidationResult result in validationResults)
+                {
+                    Console.WriteLine(result.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No errors");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
